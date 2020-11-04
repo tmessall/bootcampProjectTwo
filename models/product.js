8b83d5caee0d0b2994dcc407ec8e9d2e96b2922a
tmessall <tmessall17@gmail.com>
2020-11-04 11:55:29 -0700

    Added routes

--- models/product.js
+++ models/product.js
@@ -1,6 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
     var Product = sequelize.define("Product", {
       name: DataTypes.STRING,
+      username: DataTypes.STRING,
       description: DataTypes.STRING,
       likes: DataTypes.INT,
       dislikes: DataTypes.INT
