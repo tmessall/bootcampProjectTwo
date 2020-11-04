8332cd147f96e6cc3ceb8b7b51d8164c23043b35
tmessall <tmessall17@gmail.com>
2020-11-04 11:44:14 -0700

    Added models

---
+++ models/product.js
@@ -0,0 +1,10 @@
+module.exports = function(sequelize, DataTypes) {
+    var Product = sequelize.define("Product", {
+      name: DataTypes.STRING,
+      description: DataTypes.STRING,
+      likes: DataTypes.INT,
+      dislikes: DataTypes.INT
+    });
+    return Product;
+  };
+  
