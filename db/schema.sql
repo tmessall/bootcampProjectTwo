9664ee61d0bf0efb2175d9037ae3fa313f750a38
tmessall <tmessall17@gmail.com>
2020-11-06 10:22:38 -0700

    Added users table

--- db/schema.sql
+++ db/schema.sql
@@ -6,11 +6,19 @@ USE congo_db;
 CREATE TABLE products (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    username VARCHAR(40) NOT NULL,
+    userID INTEGER NOT NULL,
     description TEXT NOT NULL,
     likes INT default 0,
     dislikes INT default 0,
     createdAt TIMESTAMP,
     updatedAt TIMESTAMP
 );
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(40) NOT NULL,
+    password TEXT NULL,
+    createdAt TIMESTAMP,
+    updatedAt TIMESTAMP
+)
 
