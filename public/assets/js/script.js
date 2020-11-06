48f97a65de20ce85a44f8f050febc13462d97127
bishank <chandbishank@gmail.com>
2020-11-06 11:22:21 -0700

    signup page added

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -28,6 +28,40 @@ $("#submission").on("click", function (event) {
   });
 
 
+  $("#submission").on("click", function (event) {
+    console.log("click");
+    
+    var hash = CryptoJS.MD5("Message");
+     console.log(hash.toString());
+
+
+    event.preventDefault();
+
+   
+     var Uname = $("#username").val();
+     var password = $("#password").val();
+     var md5Pass = CryptoJS.MD5(password).toString();
+
+     var newUser = {
+      name: Uname,
+      password : md5Pass
+    
+    };
+      
+
+    console.log(newUser);
+
+    $.ajax("/api/user", {
+      type: "POST",
+      data: newUser
+    }).then(function () {
+
+      location.reload();
+      console.log("added a new order");
+    })
+  });
+
+
 
 
   
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,6 +7,9 @@ module.exports = function(app) {
     app.get("/login", (req, res) => {
         res.render("login");
     });
+    app.get("/signup", (req, res) => {
+        res.render("signup");
+    });
 };
 
 
