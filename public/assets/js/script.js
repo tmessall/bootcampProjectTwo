c72f7726e81930e127d4fccc95a42efa0e1b31eb
bishank <chandbishank@gmail.com>
2020-11-06 10:32:34 -0700

    md5 package added

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -4,11 +4,17 @@
 
 $("#submission").on("click", function (event) {
     console.log("click");
+    
+    var hash = CryptoJS.MD5("Message");
+     console.log(hash.toString());
+
+
     event.preventDefault();
     var newItem = {
       name: $(".product").val(),
       username : $(".Uname").val(),
-      description : $(".description").val()
+      description : $(".description").val(),
+    
     };
     console.log(newItem);
     $.ajax("/api/products", {
@@ -20,6 +26,8 @@ $("#submission").on("click", function (event) {
       console.log("added a new order");
     })
   });
+
+
 
 
   
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,13 +3,11 @@ module.exports = function(app) {
     app.get("/", (req, res) => {
         res.render("index");
     });
-};
 
-//get login page route
-module.exports = function(app) {
-    // GET homepage route
     app.get("/login", (req, res) => {
         res.render("login");
     });
 };
+
+
   
