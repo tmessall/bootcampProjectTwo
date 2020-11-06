5da415a5e2bc1b4896017bc9a4bf5c4f5a3fef15
Travis Messall <tmessall17@gmail.com>
2020-11-06 14:21:09 -0700

    Update html-routes.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,25 +3,12 @@ module.exports = function(app) {
     app.get("/", (req, res) => {
         res.render("index");
     });
-
-};
-
-//get login page route
-module.exports = function(app) {
-    // GET homepage route
+    
     app.get("/login", (req, res) => {
         res.render("login");
     });
-};
-
-
-    app.get("/login", (req, res) => {
-        res.render("login");
-    });
+    
     app.get("/signup", (req, res) => {
         res.render("signup");
     });
 };
-
-
-  
