3f22337b79dfe6feb527a6c43b6a90169c770b86
Travis Messall <tmessall17@gmail.com>
2020-11-07 16:32:04 -0700

    Update script.js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -34,12 +34,64 @@ function signUpUser() {
     // ajax call has to happen in a certain order, so this can't be separated out into another function
     // call serves to make sure no duplicate usernames
     $.ajax("/api/users", {
-      type: "POST",
-      data: newUser
-    }).then(function () {
+      type: "GET"
+    }).then(res => {
+      res.forEach(user => {
+        console.log(user.name);
+        if (user.name.toLowerCase() == $("#usernameSign").val().toLowerCase()) {
+          newUser = false;
+        }
+      });
+      if (newUser) {
+        finishSignUp();
+      } else {
+        // Clears fields and tells user the issue
+        $("#usernameSign").val("");
+        $("#password").val("");
+        $("#confirmPassword").val("");
+        $("#userTaken").show();
+      }
+    });
+  } else {
+    // Clears fields and tells user the issue
+    $("#password").val("");
+    $("#confirmPassword").val("");
+    $("#passNoMatch").show();
+  }
+}
 
-      location.reload();
-      console.log("added a new order");
-    })
+function checkPasswordMatch() {
+  return $("#password").val() == $("#confirmPassword").val();
+}
+
+// Adds new user to db
+function finishSignUp() {
+  console.log("signing up");
+  var Uname = $("#usernameSign").val();
+  var password = $("#password").val();
+  var md5Pass = CryptoJS.MD5(password).toString();
+
+  var newUser = {
+    name: Uname,
+    password: md5Pass
   };
-};
+
+  $.ajax("/api/users", {
+    type: "POST",
+    data: newUser
+  }).then(function () {
+    window.location.replace('/');
+  })
+}
+
+// Allows user to continue to main page without logging in
+$("#guestLoad").on("click", function(event) {
+  event.preventDefault(event);
+  $.ajax("/", {
+    type: "GET",
+  }).then(function (res) {
+    console.log("idk")
+    window.location.replace('/')
+  });
+});
+
